5e704f42eafed07ce7d165ae524707454108500d
777Pavel777 <145754713+777Pavel777@users.noreply.github.com>
2025-03-04 20:54:27 +0200

    Update ofer.js

--- src/js/ofer.js
+++ src/js/ofer.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ваш-проєкт.supabase.co';
-const supabaseKey = 'ваш-anon-key';
+const supabaseUrl = 'https://jtlcmpfxrjgonsyhvagw.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0bGNtcGZ4cmpnb25zeWh2YWd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExMTI1NDIsImV4cCI6MjA1NjY4ODU0Mn0.5AU0rdSg-Hunh9U20hNx7iKm42FWNFakuXy44srNcPA';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 class User {
